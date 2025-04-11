7a2082590a7be6e7aec8a7cced1a3cfa564d0853
wuaicot <wuaicot@gmail.com>
2025-04-11 13:14:44 -0400

    cors

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(cors({
   origin: process.env.CLIENT_URL,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
